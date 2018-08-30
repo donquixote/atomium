258130ec58c02db0d94cc5bdea313ebec4ce5687
Andreas Hennings <andreas@dqxtech.net>
2018-08-30 19:11:12 +0200

    #134: Split up test suites into 'unit' and 'integration' with different bootstrap.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,26 +7,8 @@
 
 require_once './vendor/autoload.php';
 
+define('DRUPAL_ROOT', dirname(__DIR__) . '/build');
+
 // Directory change necessary since Drupal often uses relative paths.
 chdir(DRUPAL_ROOT);
 require_once 'includes/bootstrap.inc';
-
-/**
- * Return template file and variables array for testing purposes.
- *
- * @param string $template_file
- *   Template file.
- * @param mixed $variables
- *   Variables array.
- *
- * @return array
- *   Template file and variables array for testing purposes.
- */
-function atomium_test_render_template($template_file, $variables) {
-  return [
-    'template' => $template_file,
-    'variables' => $variables,
-  ];
-}
-
-drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
