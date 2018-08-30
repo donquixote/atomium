6e768a0a182daf16553f1f4d5c255ad2954a2558
Andreas Hennings <andreas@dqxtech.net>
2018-08-30 19:26:59 +0200

    STUB: Add more methods in AttributesTest.

--- tests/src/Unit/AttributesTest.php
+++ tests/src/Unit/AttributesTest.php
@@ -12,6 +12,94 @@ use Symfony\Component\Yaml\Yaml;
  */
 class AttributesTest extends AbstractUnitTest {
 
+  public function _testTrouble() {
+    $attributes = new Attributes(['class' => ['one', 'two']]);
+
+    self::assertStorage(
+      ['class' => ['one' => 'one', 'two' => 'two']],
+      $attributes);
+
+    $attributes->append('class', TRUE);
+
+    self::assertStorage(
+      ['class' => true],
+      $attributes);
+
+    $attributes->append('class', ['one', 'two three']);
+
+    self::assertStorage(
+      ['class' => [true, 'one', 'two', 'three']],
+        $attributes);
+  }
+
+  public function testAppend() {
+
+    $attributes = new Attributes(['class' => ['one', 'two']]);
+
+    self::assertStorage(
+      ['class' => ['one' => 'one', 'two' => 'two']],
+      $attributes);
+
+    $attributes->append('class', ['one', 'two']);
+
+    self::assertStorage(
+      ['class' => ['one' => 'one', 'two' => 'two']],
+      $attributes);
+
+    $attributes->append('class', ['one', 'two']);
+
+    $attr_clone = clone $attributes;
+
+    self::assertStorage(
+      ['class' => ['one' => 'one', 'two' => 'two']],
+      $attributes);
+
+    self::assertSame(
+      ['class' => ['one', 'two']],
+      $attributes->toArray());
+
+    self::assertSame(
+      ' class="one two"',
+      $attributes->__toString());
+
+    self::assertSame(
+      ' class="one two"',
+      $attr_clone->__toString());
+  }
+
+  /**
+   * @param array $expected_storage
+   * @param \Drupal\atomium\Attributes $attributes
+   * @param string $message
+   */
+  private static function assertStorage(array $expected_storage, Attributes $attributes, $message = '') {
+    // Grab private property value.
+    $array = (array)$attributes;
+    $actual_storage = reset($array);
+    self::assertSame($expected_storage, $actual_storage, $message);
+  }
+
+  /**
+   * @param \Drupal\atomium\Attributes $attributes
+   * @param string $method
+   */
+  private static function assertIntegrity(Attributes $attributes, $method) {
+    $array = (array)$attributes;
+    $actual_storage = reset($array);
+    foreach ($actual_storage as $key => $value) {
+      if (is_bool($value)) {
+        continue;
+      }
+      if (is_array($value)) {
+        foreach ($value as $k => $v) {
+          self::assertSame((string)$k, $v, "Integrity fail in Attributes object at key \$['$key']['$k'] after ->$method().");
+        }
+        continue;
+      }
+      self::assertSame([], $value, "Integrity fail in Attributes object at key '$key': Value must be bool or array.");
+    }
+  }
+
   /**
    * Test class methods.
    *
@@ -23,6 +111,7 @@ class AttributesTest extends AbstractUnitTest {
     foreach ($runs as $run) {
       foreach ($run as $method => $arguments) {
         call_user_func_array([$attributes, $method], $arguments);
+        self::assertIntegrity($attributes, $method);
       }
     }
 
@@ -34,6 +123,16 @@ class AttributesTest extends AbstractUnitTest {
           array($attributes, $method),
           $item['arguments']
         );
+        self::assertIntegrity($attributes, $method);
+
+        if ($item['return'] !== $actual) {
+          # self::assertStorage(['?'], $attributes, "\$attributes->$method() STORAGE");
+        }
+
+        self::assertSame(
+          var_export($item['return'], TRUE),
+          var_export($actual, TRUE),
+          "\$attributes->$method()");
 
         expect($actual)->to->equal($item['return']);
       }
